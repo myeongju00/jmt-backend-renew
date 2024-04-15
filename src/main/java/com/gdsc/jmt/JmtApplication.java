f1b3e2f0104096daa021bca9e1e92de923a9565e
myeongju00 <j51677767@gmail.com>
2024-04-16 00:02:30 +0900

    First init

---
+++ src/main/java/com/gdsc/jmt/JmtApplication.java
@@ -0,0 +1,13 @@
+package com.gdsc.jmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmtApplication.class, args);
+	}
+
+}
