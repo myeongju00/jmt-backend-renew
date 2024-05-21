fe7d86e7fe50030b20210c6c2042578387582611
myeongju00 <j51677767@gmail.com>
2024-05-21 10:41:17 +0900

    [feat] : 구글 ID 토큰 오류 메세지 추가

--- src/main/java/com/gdsc/jmt/global/messege/AuthMessage.java
+++ src/main/java/com/gdsc/jmt/global/messege/AuthMessage.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum AuthMessage implements ResponseMessage{
     LOGIN_BAD_REQUEST("잘못된 접근입니다.", HttpStatus.BAD_REQUEST),
-    INVALID_TOKEN("ID TOKEN이 유효하지 않습니다", HttpStatus.UNAUTHORIZED);
+    INVALID_TOKEN("ID TOKEN이 유효하지 않습니다", HttpStatus.UNAUTHORIZED),
+    GOOGLE_INVALID_ID_TOKEN("구글 ID TOKEN이 유효하지 않습니다.", HttpStatus.UNAUTHORIZED);
 
     private final String message;
     private final HttpStatus status;
