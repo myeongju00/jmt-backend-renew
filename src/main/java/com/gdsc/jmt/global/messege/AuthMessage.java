338f0d7a44ed695470d50c5af3178d2d2eb00e9b
myeongju00 <j51677767@gmail.com>
2024-05-27 15:48:01 +0900

    [feat] : 애플 id토큰으로 유저 객체 반환하는 부분 추가

--- src/main/java/com/gdsc/jmt/global/messege/AuthMessage.java
+++ src/main/java/com/gdsc/jmt/global/messege/AuthMessage.java
@@ -11,7 +11,8 @@ public enum AuthMessage implements ResponseMessage{
     INVALID_TOKEN("ID TOKEN이 유효하지 않습니다", HttpStatus.UNAUTHORIZED),
     GOOGLE_INVALID_ID_TOKEN("구글 ID TOKEN이 유효하지 않습니다.", HttpStatus.UNAUTHORIZED),
     INVALID_JWT_SIGNATURE("잘못된 JWT 토큰입니다.", HttpStatus.UNAUTHORIZED),
-    INVALID_TOKEN_EXPIRED("만료된 JWT 토큰입니다.", HttpStatus.UNAUTHORIZED);
+    INVALID_TOKEN_EXPIRED("만료된 JWT 토큰입니다.", HttpStatus.UNAUTHORIZED),
+    APPLE_PUBLIC_KEY_ERROR("애플 공개 키를 받아올 수 없습니다.", HttpStatus.INTERNAL_SERVER_ERROR),;
 
     private final String message;
     private final HttpStatus status;
