bafb2510748bebf5f791961d7259112ab1cbeea3
myeongju00 <j51677767@gmail.com>
2024-07-03 16:32:39 +0900

    [feat] : user 서비스 테스트 코드 작성

--- src/test/java/com/gdsc/jmt/domain/user/service/UserServiceTest.java
+++ src/test/java/com/gdsc/jmt/domain/user/service/UserServiceTest.java
@@ -1,71 +1,55 @@
 package com.gdsc.jmt.domain.user.service;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.gdsc.jmt.domain.user.dao.UserDao;
-import com.gdsc.jmt.domain.user.entity.UserEntity;
-import com.gdsc.jmt.domain.user.entity.common.RoleType;
-import com.gdsc.jmt.domain.user.entity.common.SocialType;
-import com.gdsc.jmt.domain.user.entity.common.Status;
-import com.gdsc.jmt.domain.user.oauth.info.OAuth2UserInfo;
-import com.gdsc.jmt.domain.user.oauth.info.impl.AppleOAuth2UserInfo;
-import com.gdsc.jmt.domain.user.repository.UserRepository;
+import com.gdsc.jmt.global.service.S3FileService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
-    @InjectMocks
-    private UserService userService;
-
-    @InjectMocks
+    @Mock
     private UserDao userDao;
 
     @Mock
-    private UserRepository userRepository;
+    private S3FileService s3FileService;
 
-    private final String email = "test@test.com";
-    private final String nickname = "testNickname";
-    private UserEntity userEntity;
+    @InjectMocks
+    private UserService userService;
 
     @BeforeEach
     void setUp() {
-        userDao = new UserDao(userRepository);
-        userEntity = UserEntity.builder()
-                .email(email)
-                .socialType(SocialType.TEST)
-                .roleType(RoleType.MEMBER)
-                .status(Status.ACTIVE)
-                .build();
-        userEntity.updateNickname(nickname);
-
-        userDao.saveTestUser(userEntity);
+        MockitoAnnotations.openMocks(this);
     }
 
+    @DisplayName("닉네임 업데이트 - 성공")
     @Test
-    void updateUserNickName() {
-        String changedNickName = "changedNickName";
-//
-//        String expected = userDao.updateNickname(email, changedNickName);
-//
-//        assertNotNull(expected);
-//        assertNotEquals(expected, nickname);
-//        assertEquals(expected, changedNickName);
-//        when(userDao.saveTestUser(userEntity)).thenReturn(1L);
+    void updateUserNickName_Success() {
+        String email = "test@example.com";
+        String newNickName = "newNickName";
 
-        doReturn(changedNickName).when(userDao.updateNickname(anyString(), changedNickName));
+        when(userDao.isExistNickname(newNickName)).thenReturn(false);
+        when(userDao.updateNickname(email, newNickName)).thenReturn(newNickName);
+
+        String result = userService.updateUserNickName(email, newNickName);
+
+        assertEquals(newNickName, result);
+        verify(userDao, times(1)).isExistNickname(newNickName);
+        verify(userDao, times(1)).updateNickname(email, newNickName);
     }
+
 
     @Test
     void updateUserProfileImg() {
